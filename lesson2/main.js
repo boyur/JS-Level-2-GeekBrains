68cd9c299baabb5fca6e5515b8358d3bc5535a79
Artem Boyur <gboyur@gmail.com>
2016-12-25 16:23:51 +0300

    add load json

--- lesson2/main.js
+++ lesson2/main.js
@@ -2,6 +2,8 @@ class Chess {
 
   constructor(div) {
     this.div = document.querySelector(div);
+    this.startPosition = {};
+    this.history = {};
   }
 
   // Генерация шахматной доски
@@ -137,150 +139,29 @@ class Chess {
 
   // Вешаем события на все фигуры
   arrangeChees() {
-    let startPosition = {
-      "white": {
-        "w-pawn1": {
-          "pos": "A2",
-          "class": "figurePawnWhite"
-        },
-        "w-pawn2": {
-          "pos": "B2",
-          "class": "figurePawnWhite"
-        },
-        "w-pawn3": {
-          "pos": "C2",
-          "class": "figurePawnWhite"
-        },
-        "w-pawn4": {
-          "pos": "D2",
-          "class": "figurePawnWhite"
-        },
-        "w-pawn5": {
-          "pos": "E2",
-          "class": "figurePawnWhite"
-        },
-        "w-pawn6": {
-          "pos": "F2",
-          "class": "figurePawnWhite"
-        },
-        "w-pawn7": {
-          "pos": "G2",
-          "class": "figurePawnWhite"
-        },
-        "w-pawn8": {
-          "pos": "H2",
-          "class": "figurePawnWhite"
-        },
-        "w-rook1": {
-          "pos": "A1",
-          "class": "figureRookWhite"
-        },
-        "w-horse1": {
-          "pos": "B1",
-          "class": "figureHorseWhite"
-        },
-        "w-elephant1": {
-          "pos": "C1",
-          "class": "figureElephantWhite"
-        },
-        "w-queen": {
-          "pos": "D1",
-          "class": "figureQueenWhite"
-        },
-        "w-king": {
-          "pos": "E1",
-          "class": "figureKingWhite"
-        },
-        "w-elephant2": {
-          "pos": "F1",
-          "class": "figureElephantWhite"
-        },
-        "w-horse2": {
-          "pos": "G1",
-          "class": "figureHorseWhite"
-        },
-        "w-rook2": {
-          "pos": "H1",
-          "class": "figureRookWhite"
-        }
-      },
-      "black": {
-        "b-pawn1": {
-          "pos": "A7",
-          "class": "figurePawnBlack"
-        },
-        "b-pawn2": {
-          "pos": "B7",
-          "class": "figurePawnBlack"
-        },
-        "b-pawn3": {
-          "pos": "C7",
-          "class": "figurePawnBlack"
-        },
-        "b-pawn4": {
-          "pos": "D7",
-          "class": "figurePawnBlack"
-        },
-        "b-pawn5": {
-          "pos": "E7",
-          "class": "figurePawnBlack"
-        },
-        "b-pawn6": {
-          "pos": "F7",
-          "class": "figurePawnBlack"
-        },
-        "b-pawn7": {
-          "pos": "G7",
-          "class": "figurePawnBlack"
-        },
-        "b-pawn8": {
-          "pos": "H7",
-          "class": "figurePawnBlack"
-        },
-        "b-rook1": {
-          "pos": "A8",
-          "class": "figureRookBlack"
-        },
-        "b-horse1": {
-          "pos": "B8",
-          "class": "figureHorseBlack"
-        },
-        "b-elephant1": {
-          "pos": "C8",
-          "class": "figureElephantBlack"
-        },
-        "b-queen": {
-          "pos": "D8",
-          "class": "figureQueenBlack"
-        },
-        "b-king": {
-          "pos": "E8",
-          "class": "figureKingBlack"
-        },
-        "b-elephant2": {
-          "pos": "F8",
-          "class": "figureElephantBlack"
-        },
-        "b-horse2": {
-          "pos": "G8",
-          "class": "figureHorseBlack"
-        },
-        "b-rook2": {
-          "pos": "H8",
-          "class": "figureRookBlack"
-        }
+    // Загрузка стартовых позиций
+    let p = new Promise(function (resolve) {
+      let req = new XMLHttpRequest();
+      // Рассположение файла
+      req.open('GET', './startPosition.json', true);
+      req.onload = function() {
+        resolve(JSON.parse(req.response));
+      };
+      req.send();
+    });
+
+    p.then((obj) => {
+      this.startPosition = obj;
+      console.log(obj);
+
+      for (let key in this.startPosition["white"]) {
+        this.creatFigure(key, this.startPosition["white"]);
       }
-    };
 
-    console.log(startPosition["white"]);
-
-    for (let key in startPosition["white"]) {
-      this.creatFigure(key, startPosition["white"]);
-    }
-
-    for (let key in startPosition["black"]) {
-      this.creatFigure(key, startPosition["black"]);
-    }
+      for (let key in this.startPosition["black"]) {
+        this.creatFigure(key, this.startPosition["black"]);
+      }
+    });
   }
 
 }
@@ -289,106 +170,3 @@ const chess = new Chess('#board');
 
 chess.createBoard();
 chess.arrangeChees();
-
-// let test = document.querySelector('td[data-position="A1"]');
-// console.log(test);
-// let test2 = document.createElement('div');
-// test2.classList.add('figureKingWhite');
-// test.appendChild(test2);
-//
-// test = document.querySelector('td[data-position="B1"]');
-// console.log(test);
-// test2 = document.createElement('div');
-// test2.classList.add('figureQueenWhite');
-// test.appendChild(test2);
-
-
-
-// function creatFigure(idFigure, obj) {
-//   let div = document.createElement('div');
-//   div.id = idFigure;
-//   div.classList.add('figure');
-//   div.classList.add(obj[idFigure]['class']);
-//   div.style.top = '0px';
-//   div.style.left = '0px';
-//   let td = document.querySelector(`td[data-position="${obj[idFigure]['pos']}"]`);
-//   console.log(td);
-//   td.appendChild(div);
-//
-//   console.log(`Фигура ${idFigure} создана!`);
-//
-//   addEvents(div);
-// }
-//
-// function addEvents(div) {
-//   // Вешаю обработчики
-//   let targetItem = {};
-//   let targetOffset = {};
-//   let flag = false;
-//
-//   // Зажатие клавиши мыши
-//   div.addEventListener("mousedown", function (e) {
-//
-//     if (e.target.tagName == "BODY" || e.target.tagName == "TABLE") {
-//       return;
-//     }
-//
-//     flag = true;
-//
-//     targetItem = e.target;
-//
-//     console.log(targetItem);
-//
-//     targetOffset.x = e.clientX - e.target.style.left.replace(/[^-0-9]/gim,'');
-//     targetOffset.y = e.clientY - e.target.style.top.replace(/[^-0-9]/gim,'');
-//
-//     console.log(`x: ${e.target.style.top}; y:${e.target.style.left}`);
-//     console.log(targetOffset);
-//   });
-//
-//   // Движение мыши
-//   div.addEventListener("mousemove", function (e) {
-//     if (!flag) {
-//       return;
-//     }
-//
-//     let target = e.target;
-//
-//     console.log(targetItem);
-//
-//     targetItem.style.top = e.clientY - targetOffset.y + "px";
-//     targetItem.style.left = e.clientX - targetOffset.x + "px";
-//     console.log(e.clientX);
-//     console.log(targetOffset.x);
-//     console.log(target.style.top);
-//   });
-//
-//   // Отпустить мыш
-//   div.addEventListener("mouseup", function (e) {
-//     let target = e.target;
-//     let position = false;
-//     flag = false;
-//     targetItem = null;
-//     console.log(target);
-//
-//     // Смотрим что за позиция
-//     let elem = document.elementFromPoint(e.clientX, e.clientY);
-//
-//     if (elem.tagName != 'TD') {
-//       elem.style.display = 'none';
-//       position = document.elementFromPoint(e.clientX, e.clientY).getAttribute('data-position');
-//       console.log(position);
-//       elem.style.display = 'block';
-//     }
-//
-//     // Меняем позицию
-//     console.log(e.target);
-//     e.target.remove();
-//
-//     let td = document.querySelector(`td[data-position="${position}"]`);
-//     console.log(td);
-//     td.appendChild(target);
-//     target.style.top = '0px';
-//     target.style.left = '0px';
-//   });
-// }
